150b3ea38c6d53046a27a8c6ff9a2ba84fcae158
Andy Williams <andy@andy.xyz>
2025-06-16 12:18:20 +0100

    Update to latest builder app

--- main.gui.go
+++ main.gui.go
@@ -10,25 +10,25 @@ import (
 )
 
 type gui struct {
-	out *widget.Label
-	bc  *widget.Button
-	bbo *widget.Button
-	bbc *widget.Button
-	bd  *widget.Button
-	b7  *widget.Button
-	b8  *widget.Button
-	b9  *widget.Button
-	bm  *widget.Button
-	b4  *widget.Button
-	b5  *widget.Button
-	b6  *widget.Button
-	bs  *widget.Button
+	b0  *widget.Button
 	b1  *widget.Button
 	b2  *widget.Button
 	b3  *widget.Button
+	b4  *widget.Button
+	b5  *widget.Button
+	b6  *widget.Button
+	b7  *widget.Button
+	b8  *widget.Button
+	b9  *widget.Button
 	ba  *widget.Button
-	b0  *widget.Button
+	bbc *widget.Button
+	bbo *widget.Button
+	bc  *widget.Button
+	bd  *widget.Button
+	bm  *widget.Button
 	bp  *widget.Button
+	bs  *widget.Button
+	out *widget.Label
 }
 
 func newGUI() *gui {
@@ -36,25 +36,25 @@ func newGUI() *gui {
 }
 
 func (g *gui) makeUI() fyne.CanvasObject {
-	g.bbo = widget.NewButton("(", func() {})
-	g.b9 = widget.NewButton("9", func() {})
+	g.b0 = widget.NewButton("0", func() {})
 	g.b1 = widget.NewButton("1", func() {})
-	g.ba = widget.NewButton("+", func() {})
-	g.bc = &widget.Button{Text: "C", Importance: 4, OnTapped: func() {}}
-	g.bbc = widget.NewButton(")", func() {})
-	g.b7 = widget.NewButton("7", func() {})
-	g.b6 = widget.NewButton("6", func() {})
-	g.bs = widget.NewButton("-", func() {})
-	g.bm = widget.NewButton("*", func() {})
+	g.b2 = widget.NewButton("2", func() {})
 	g.b3 = widget.NewButton("3", func() {})
-	g.bp = widget.NewButton(".", func() {})
-	g.out = widget.NewLabel("0")
-	g.bd = widget.NewButton("/", func() {})
-	g.b8 = widget.NewButton("8", func() {})
 	g.b4 = widget.NewButton("4", func() {})
 	g.b5 = widget.NewButton("5", func() {})
-	g.b2 = widget.NewButton("2", func() {})
-	g.b0 = widget.NewButton("0", func() {})
+	g.b6 = widget.NewButton("6", func() {})
+	g.b7 = widget.NewButton("7", func() {})
+	g.b8 = widget.NewButton("8", func() {})
+	g.b9 = widget.NewButton("9", func() {})
+	g.ba = widget.NewButton("+", func() {})
+	g.bbc = widget.NewButton(")", func() {})
+	g.bbo = widget.NewButton("(", func() {})
+	g.bc = &widget.Button{Text: "C", Importance: 4, Alignment: 0, OnTapped: func() {}}
+	g.bd = widget.NewButton("/", func() {})
+	g.bm = widget.NewButton("*", func() {})
+	g.bp = widget.NewButton(".", func() {})
+	g.bs = widget.NewButton("-", func() {})
+	g.out = widget.NewLabel("0")
 
 	return container.NewGridWithColumns(1,
 		g.out,
@@ -82,5 +82,5 @@ func (g *gui) makeUI() fyne.CanvasObject {
 			container.NewGridWithColumns(2,
 				g.b0,
 				g.bp),
-			&widget.Button{Text: "=", Importance: 1, OnTapped: g.evaluate}))
+			&widget.Button{Text: "=", Importance: 1, Alignment: 0, OnTapped: g.evaluate}))
 }
